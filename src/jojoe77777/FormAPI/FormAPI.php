3c18afa891f65bc5f2c00d844c9da92d44b40128
IamFxr <128105244+ShxdowBadDev@users.noreply.github.com>
2023-07-30 21:05:03 -0700

    Add files via upload

---
+++ src/jojoe77777/FormAPI/FormAPI.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace jojoe77777\FormAPI;
+
+use pocketmine\plugin\PluginBase;
+
+class FormAPI extends PluginBase{
+
+
+    public function createSimpleForm(?callable $function = null) : SimpleForm {
+        return new SimpleForm($function);
+    }
+
+   
